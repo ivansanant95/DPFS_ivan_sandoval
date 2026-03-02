5407560d4f616174820a8a91b344864877abc7a8
ivan <ivansanant95@gmail.com>
2026-03-02 18:29:16 -0300

    Se configuro la Session, Cookies y Dependencias de NPM (bcrypt, multer)

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 
 const methodOverride = require('method-override'); // Permite usar PUT y DELETE
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +12,14 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false })); // Permite capturar información de formularios (req.body)
 app.use(express.json()); // Permite capturar JSON
 app.use(methodOverride('_method')); // Permite pisar el método POST por PUT/DELETE (?_method=PUT)
+
+// Configuración del manejo de Sesiones y Cookies
+app.use(session({
+    secret: 'ReparatechSecretKey2026', // Clave privada para firmar la sesión
+    resave: false,                     // No guarda la sesión si no hubo cambios
+    saveUninitialized: false           // No guarda sesiones vacías
+}));
+app.use(cookieParser());
 
 // Configuración de recursos estáticos (CSS, Imágenes)
 app.use(express.static(path.join(__dirname, '../public')));
